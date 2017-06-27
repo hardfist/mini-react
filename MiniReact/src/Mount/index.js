bbb8fa3c924fe23947a9397a0adb4c204b72e33c
yangjian.fe <yangjian.fe@bytedance.com>
2017-06-27 20:05:27 +0800

    完成实例化
    Change-Id: I8ce66e0fa72c1facd5718a062292e5900f684ed5

--- MiniReact/src/Mount/index.js
+++ MiniReact/src/Mount/index.js
@@ -2,55 +2,99 @@ import Component from '../Component';
 function isClass(type){
   return type.prototype && type.prototype instanceof Component;
 }
-function mountComposite(element){
-  const { type, props } = element;
-  let  renderedElement;
-  if(isClass(type)){
-    let instance = new type(props);
-    instance.props = props;
-    if(instance.componentWillMount){
-      instance.componentWillMount();
-    }
-    renderedElement = instance.render();
-  }else{
-    renderedElement = type(props);
+class CompositeComponent{
+  constructor(element){
+    this.currentElement = element;
+    this.renderedComponent = null;
+    this.publicInstance = null;
   }
-  return mount(renderedElement);
-}
-function mountHost(element){
-  const { type, props } = element;
-  const children = props.children;
-  const node = document.createElement(type);
-  Object.keys(props).forEach(propName => {
-    if(propName !== 'children'){
-      node.setAttribute(propName,props[propName]);
+  getPublicInstance(){
+    return this.publicInstance;
+  }
+  mount(){
+    const element = this.currentElement;
+    const { type, props } = element;
+
+    let publicInstance;
+    let renderedElement;
+    if(isClass(type)){
+      publicInstance = new type(props);
+      publicInstance.props = props;
+      if(publicInstance.componentWillMount){
+        publicInstance.componentWillMount();
+      }
+      renderedElement = publicInstance.render();
+    }else if(typeof type === 'function'){
+      publicInstance = null;
+      renderedElement = type(props);
     }
-  })
-  children.forEach(childElement => {
-    const childNode = mount(childElement);
-    node.appendChild(childNode);
-  })
-  return node;
+    this.publicInstance = publicInstance;
+
+    const renderedComponent = instantiateComponent(renderedElement);
+    this.renderedComponent = renderedComponent;
+    return renderedComponent.mount();
+  }
 }
-function mountText(text){
-  const node = document.createElement('span')
-  node.textContent = text;
-  return node;
+
+class DOMComponent {
+  constructor(element){
+    this.currentElement = element;
+    this.renderedChildren = [];
+    this.node = null;
+  }
+  getPublicInstance(){
+    return this.node;
+  }
+  mount(){
+    const element = this.currentElement;
+    const { type, props } = element;
+    const children = props.children;
+    const node = document.createElement(type);
+    this.node = node;
+
+    Object.keys(props).forEach(propName => {
+      if(propName !== 'children'){
+        node.setAttribute(propName,props[propName])
+      }
+    })
+
+    const renderedChildren = children.map(instantiateComponent);
+    this.renderedChildren = renderedChildren;
+
+    const childNodes = renderedChildren.map(child => child.mount());
+    childNodes.forEach(childNode => node.appendChild(childNode));
+    return node;
+  }
 }
-function mount(element){
+class TextComponent{
+  constructor(text){
+    this.currentText = text;
+  }
+  mount(){
+    const node = document.createElement('span');
+    node.textContent = this.currentText;
+    this.node = node;
+    return node;
+  }
+}
+function instantiateComponent(element){
   if(typeof element === 'string'){
-    return mountText(element);
+    return new TextComponent(element);
   }
   const type = element.type;
   if(typeof type === 'function'){
-    return mountComposite(element);
-  }else{
-    return mountHost(element);
+    return new CompositeComponent(element);
+  }else if( typeof type === 'string'){
+    return new DOMComponent(element);
   }
 }
+
 function render(element,containerNode){
-  const node = mount(element);
+  const rootComponent = instantiateComponent(element);
+  const node = rootComponent.mount();
   containerNode.appendChild(node);
+  const publicInstance = rootComponent.getPublicInstance();
+  return publicInstance;
 }
 export default {
   render
